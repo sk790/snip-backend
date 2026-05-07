750892bb47c5088960d5cf69bb7d3c5819adac4c
Saurabh Nagar <saurabhk2890@gmail.com>
2026-05-07 21:46:53 +0530

    first commit

---
+++ src/models/Url.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUrl extends Document {
+  originalUrl: string;
+  shortUrl: string;
+  urlCode: string;
+  createdAt: Date;
+  startDate?: Date;
+  expiresAt?: Date;
+  password?: string;
+  userId?: mongoose.Types.ObjectId;
+  isDeleted: boolean;
+  isQr: boolean;
+  isCustom: boolean;
+}
+
+const UrlSchema: Schema = new Schema({
+  originalUrl: { type: String, required: true },
+  shortUrl: { type: String, required: true },
+  urlCode: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now },
+  startDate: { type: Date, default: Date.now },
+  expiresAt: { type: Date },
+  password: { type: String },
+  userId: { type: Schema.Types.ObjectId, ref: 'User' },
+  isDeleted: { type: Boolean, default: false },
+  isQr: { type: Boolean, default: false },
+  isCustom: { type: Boolean, default: false },
+});
+
+export default mongoose.model<IUrl>('Url', UrlSchema);
